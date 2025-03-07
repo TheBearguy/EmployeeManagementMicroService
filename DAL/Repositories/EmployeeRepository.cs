dca49387c44e37739695bf6664cd6b8b10180ae9
TheBearguy <bugbreaker18@gmail.com>
2025-03-07 14:02:22 +0530

    added entire CRUD for employees in DAL

--- DAL/Repositories/EmployeeRepository.cs
+++ DAL/Repositories/EmployeeRepository.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DomainModels; 
+using DomainModels;
+using System.Data.SqlClient;
 
 namespace DAL.Repositories
 {
@@ -37,6 +38,59 @@ namespace DAL.Repositories
                 }
             }
             return employees;
+        }
+        public Employee GetById()
+        {
+            var employee = new Employee();
+            var query = "EXEC ManageEmployees 'SELECT', @Id";
+            var reader = _databaseHelper.ExecuteReader(query);
+            using (reader)
+            {
+                if (reader.Read())
+                {
+                    return new Employee
+                    {
+                        Id = (int)reader["Id"],
+                        Name = reader["Name"].ToString(),
+                        Position = reader["position"].ToString(),
+                        Salary = Convert.ToDecimal(reader["Salary"])
+                    };
+                }
+            }
+            return null;
+        }
+
+        public void Insert(Employee employee)
+        {
+            var query = "EXEC ManageEmployees 'INSERT', @Name, @Position, @Salary";
+            var parameters = new SqlParameter[]
+            {
+                new SqlParameter("@Name", employee.Name),
+                new SqlParameter("@Position", employee.Position),
+                new SqlParameter("@Salary", employee.Salary)
+            };
+            _databaseHelper.ExecuteNonQuery(query, parameters);
+        }
+        public void Update(Employee employee)
+        {
+            var query = "EXEC ManageEmployees 'UPDATE', @Id, @Name, @Position, @Salary";
+            var parameters = new SqlParameter[]
+            {
+                new SqlParameter("@Id", employee.Id),
+                new SqlParameter("@Name", employee.Name),
+                new SqlParameter("@Position", employee.Position),
+                new SqlParameter("@Salary", employee.Salary)
+            };
+            _databaseHelper.ExecuteNonQuery(query, parameters);
+        }
+        public void Delete(int id)
+        {
+            var query = "EXEC ManageEmployees 'DELETE', @Id";
+            var parameters = new SqlParameter[]
+            {
+                new SqlParameter("@Id", id)
+            };
+            _databaseHelper.ExecuteNonQuery(query, parameters);
         }
     }
 }
