1a6b8e2547059989b025f7d863084897ad5a4099
TheBearguy <bugbreaker18@gmail.com>
2025-03-07 13:54:16 +0530

    added get employees method in the repository.

---
+++ DAL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,42 @@
+﻿using DAL.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModels; 
+
+namespace DAL.Repositories
+{
+    class EmployeeRepository
+    {
+        private readonly DatabaseHelper _databaseHelper;
+        public EmployeeRepository(string connectionString)
+        {
+            _databaseHelper = new DatabaseHelper(connectionString);
+        }
+        
+        public List<Employee> GetAll()
+        {
+            var employees = new List<Employee>();
+            var query = "EXEC ManageEmployees 'SELECT'";
+            var reader = _databaseHelper.ExecuteReader(query);
+            using (reader)
+            {
+                while ( reader.Read())
+                {
+                    employees.Add(
+                        new Employee
+                        {
+                            Id = (int)reader["Id"],
+                            Name = reader["Name"].ToString(),
+                            Position = reader["position"].ToString(),
+                            Salary = Convert.ToDecimal(reader["Salary"])
+                        }
+                    );
+                }
+            }
+            return employees;
+        }
+    }
+}
--- DomainModels/Employee.cs
+++ DomainModels/Employee.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Position { get; set; }
-        public decimal String { get; set; }
+        public decimal Salary { get; set; }
     }
 }
